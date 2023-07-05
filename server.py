ae216df0f3c550934d2a9d3c86bf7dd4fcfbe493
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-07-05 12:39:44 +0530

    feat: listing uploaded files and ability to download files

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
-import os
-from flask import Flask, render_template, request
+from os import path, listdir
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', files=listdir('uploads'))
+
 
 @app.post('/upload')
 def upload():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        uploaded_file.save(os.path.join('uploads',uploaded_file.filename))
+        uploaded_file.save(path.join('uploads', uploaded_file.filename))
     return render_template('index.html')
+
+@app.get('/uploads/<filename>')
+def download(filename):
+    return send_from_directory('uploads', filename)
 
 
 if __name__ == '__main__':
