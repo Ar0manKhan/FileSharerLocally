07f14c3c42e8c41a4f92b4e78debd531335d4021
Arman Khan <73093255+Ar0manKhan@users.noreply.github.com>
2024-05-05 15:56:41 +0530

    some msg

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from os import path, listdir
+import os
 from flask import Flask, redirect, render_template, request, send_from_directory
+import flask
 
 
 def get_local_ip():
@@ -15,20 +17,30 @@ HOST = get_local_ip()
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    print(f'{request.host_url=}')
-    return render_template(
-        'index.html',
-        files=listdir('uploads'),
-        address=f'http://{HOST}:{PORT}'
-    )
+@app.route('/', defaults={"user_path": ""})
+@app.route('/<path:user_path>')
+def home(user_path: str):
+    current_path = "./"
+    cwd = os.getcwd()
+    if user_path != "":
+        current_path = path.join(cwd, user_path)
+
+    if not path.exists(current_path):
+        return "404"
+    if path.isdir(current_path):
+        return render_template(
+            'index.html',
+            files=map(lambda x: {"url": path.join(user_path, x), "name": x},listdir(current_path)),
+            address=f'http://{HOST}:{PORT}'
+        )
+    else:
+        return flask.send_file(current_path)
 
 
 @app.post('/upload')
 def upload():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
+    if uploaded_file.filename != None:
         uploaded_file.save(path.join('uploads', uploaded_file.filename))
     return redirect('/')
 
